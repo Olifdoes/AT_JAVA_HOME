5bf0e40505685a8b5498e852e61dd73d6a887570
Olifdoes <cherdaktumanniy@yandex.ru>
2026-05-18 16:26:00 +0300

    Add files via upload

---
+++ delivery/model/ExpressParcel.java
@@ -0,0 +1,35 @@
+package model;
+
+public class ExpressParcel extends Parcel {
+
+    private int deliveryHours;
+
+    public ExpressParcel(String recipientName,
+                         String deliveryAddress,
+                         double weight,
+                         String trackingNumber,
+                         int deliveryHours) {
+
+        super(recipientName, deliveryAddress, weight, trackingNumber);
+
+        this.deliveryHours = deliveryHours;
+    }
+
+    @Override
+    public double calculateDeliveryPrice() {
+
+        double price = super.calculateDeliveryPrice();
+
+        if (deliveryHours < 24) {
+            price += 500;
+        }
+
+        return price;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Delivery deadline: " + deliveryHours + " hours");
+    }
+}
---
+++ delivery/model/FragileParcel.java
@@ -0,0 +1,28 @@
+package model;
+
+public class FragileParcel extends Parcel {
+
+    private boolean requiresCarefulHandling;
+
+    public FragileParcel(String recipientName,
+                         String deliveryAddress,
+                         double weight,
+                         String trackingNumber,
+                         boolean requiresCarefulHandling) {
+
+        super(recipientName, deliveryAddress, weight, trackingNumber);
+
+        this.requiresCarefulHandling = requiresCarefulHandling;
+    }
+
+    @Override
+    public double calculateDeliveryPrice() {
+        return super.calculateDeliveryPrice() + 200;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Handle with care: " + requiresCarefulHandling);
+    }
+}
